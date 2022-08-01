f0b7c3ec1e666273a0596ef3b2c3cb8e95613456
Denilson Bonatti <denilsonbonatti@gmail.com>
2022-08-01 16:49:10 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+
+<head>
+<title>Exemplo PHP</title>
+</head>
+<body>
+
+<?php
+ini_set("display_errors", 1);
+header('Content-Type: text/html; charset=iso-8859-1');
+
+
+
+echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+
+$servername = "";
+$username = "root";
+$password = "Senha123";
+$database = "meubanco";
+
+// Criar conexão
+
+
+$link = new mysqli($servername, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$valor_rand1 =  rand(1, 999);
+$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+$host_name = gethostname();
+$host_ip = $_SERVER['REMOTE_ADDR'];
+
+
+$query = "INSERT INTO dados (ID, data1, data2, Host, ip) VALUES ('$valor_rand1' , '$valor_rand2', '$host_name', $host_ip)";
+
+
+if ($link->query($query) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $link->error;
+}
+
+?>
+</body>
+</html>
