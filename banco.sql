3dec27565048caf8ebdf7e576b7ab080a136df9b
Denilson Bonatti <denilsonbonatti@gmail.com>
2022-08-01 17:54:47 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,7 +1,7 @@
 CREATE TABLE dados (
     id int,
-    data1 varchar(20),
-    data2 varchar(20),
-    host varchar(20),
-    ip varchar(20)
+    data1 varchar(50),
+    data2 varchar(50),
+    hostname varchar(50),
+    ip varchar(50)
 );
